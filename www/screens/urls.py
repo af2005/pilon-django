efaf988a3920777af86dfff19cf704128aae15e9
af2005 <10899721+af2005@users.noreply.github.com>
2021-01-31 23:31:38 +0100

    wip

--- www/screens/urls.py
+++ www/screens/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 
 from . import project, people, admin, login, content, dashboard
+from django.contrib.auth import views as auth_views
 
 from .admin import main
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name="www/registration/login.html")),
     path('', dashboard.view_dashboard, name='Dashboard'),
     path('login', login.view_login, name="Login"),
     path('logout', login.view_logout, name="Logout"),
@@ -15,6 +17,5 @@ urlpatterns = [
     path('people', people.directory, name="View People"),
 
     path('system-settings/<str:setting>', admin.main.system_settings, name="System settings")
-
 
 ]
