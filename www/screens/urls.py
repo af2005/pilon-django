18f55c99fd96fa5a706f5b16bd5d1e627f1dffb9
af2005 <10899721+af2005@users.noreply.github.com>
2021-02-06 15:44:10 +0100

    rearanged paths again

--- www/screens/urls.py
+++ www/screens/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 
-from . import project, people, admin, content, dashboard, user_settings
+from . import people, admin, user_settings
 from django.contrib.auth import views as auth_views
 
 from .admin import main
 
 urlpatterns = [
-    path('', dashboard.view_dashboard, name='Dashboard'),
+    path('', include('www.screens.dashboard.urls')),
     path('accounts/login/',
          auth_views.LoginView.as_view(
              template_name="www/accounts/login.html")
@@ -20,6 +20,5 @@ urlpatterns = [
     path('p/', include('www.screens.project.urls')),
     path('people', people.directory, name="View People"),
     path('system-settings/<str:setting>', admin.main.system_settings, name="System settings"),
-    path('dashboard/all-updates', dashboard.view_all_updates_board, name="All updates board"),
-    path('dashboard/last-worked-on', dashboard.view_recently_worked_on_board, name="Last worked on board"),
+
 ]
