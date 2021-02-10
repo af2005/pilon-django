805b550f84023f2f9fb58f08a38bfc67480e7405
af2005 <10899721+af2005@users.noreply.github.com>
2021-02-10 23:26:41 +0100

    add database tree

--- www/models.py
+++ www/models.py
@@ -1,7 +1,7 @@
 import uuid
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 from mptt.models import MPTTModel, TreeForeignKey
 
@@ -11,61 +11,83 @@ from markdownfield.validators import VALIDATOR_STANDARD
 from datetime import datetime
 
 
-class Content(MPTTModel):
+class Entity(MPTTModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=200)
+    name = models.CharField(max_length=255)
     parent = TreeForeignKey(
         "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
     )
     date_created = models.DateTimeField(default=datetime.now)
     date_modified = models.DateTimeField(default=datetime.now)
-    author = ""
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="creator"
+    )
 
     class MPTTMeta:
-        order_insertion_by = ["title"]
+        order_insertion_by = ["name"]
 
 
-class Child(Content):
+class Child(Entity):
     space_key = models.CharField(max_length=20)
 
 
-class Project(models.Model):
+class Project(Entity):
     key = models.CharField(max_length=20, primary_key=True)
-    name = models.CharField(max_length=50)
 
 
-class Page(models.Model):
-    # project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    id = models.IntegerField(primary_key=True)
-    # version = models.IntegerField(default=0)
-    content = MarkdownField(
+class MarkdownEntity(Entity):
+    markdown = MarkdownField(
         default="",
         rendered_field="content_rendered",
         validator=VALIDATOR_STANDARD,
         use_editor=False,
         use_admin_editor=True,
     )
-    content_rendered = RenderedMarkdownField(default="")
+    markdown_rendered = RenderedMarkdownField(default="")
 
     def repr(self):
         return {"id": self.id}
 
 
-class WikiPage(Page):
+class WikiPage(MarkdownEntity):
     pass
 
 
-class JournalPage(Page):
+class JournalPage(MarkdownEntity):
     date = models.DateTimeField()
 
 
-class Task(models.Model):
-    created_date = models.DateTimeField()
+class Task(MarkdownEntity):
     due_date = models.DateTimeField()
-    reporter = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="Reporter"
-    )
     assignee = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="Assignee"
     )
     content = models.TextField()
+
+
+class Comment(MarkdownEntity):
+    pass
+
+
+class Attachment(Entity):
+    RENDERABLE_FILE_TYPES = (
+        "png",
+        "gif",
+        "jpeg",
+        "jpg",
+        "pdf",
+        "svg",
+        #"eps",
+        "html",
+        "docx",
+        "pptx",
+        "bmp",
+    )
+
+    file_name = models.CharField(max_length=255)
+    file_type = models.CharField(max_length=255)
+    file_path = "" # todo create uuid
+
+    @property
+    def renderable(self):
+        return self.file_type in self.RENDERABLE_FILE_TYPES
