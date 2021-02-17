2e4b5465b693d68f73a82811a9887a00f27de8be
af2005 <10899721+af2005@users.noreply.github.com>
2021-02-17 22:51:11 +0100

    add serialisers

--- www/rest/serializers.py
+++ www/rest/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
-from ..models import Entity
+from ..models import Entity, Project, MarkdownEntity, Task, Attachment
 
 
 class EntitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Entity
         fields = ("id", "name", "parent", "date_created", "date_modified", "creator")
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = "key"
+
+
+class MarkdownEntitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MarkdownEntity
+        fields = ("markdown", "markdown_rendered")
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ("due_date", "assignee")
+
+
+class AttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attachment
+        fields = ("file_name", "file_type", "file_path")
