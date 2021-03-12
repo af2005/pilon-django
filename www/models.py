dccc28d5e7002b291de5dde80f2a073f49e2911f
af2005 <10899721+af2005@users.noreply.github.com>
2021-03-12 21:43:29 +0100

    add hyphens to urls

--- www/models.py
+++ www/models.py
@@ -29,7 +29,7 @@ class Entity(PolymorphicMPTTModel):
     parent = PolymorphicTreeForeignKey(
         "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
     )
-    date_created = models.DateTimeField(default=timezone.now)
+    date_created = models.DateTimeField(default=timezone.now, editable=False)
     date_modified = models.DateTimeField(default=timezone.now)
     creator = models.ForeignKey(
         User,
