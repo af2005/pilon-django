0d16274508c3b4c4a216ba547d39fcb318e63f2a
af2005 <10899721+af2005@users.noreply.github.com>
2021-03-13 11:38:04 +0100

    semi automatic icon patched for editor

--- www/static/www/bootstrap-icons/patch_icons.py
+++ www/static/www/bootstrap-icons/patch_icons.py
@@ -1,15 +1,27 @@
-
-
 css_classes_to_extend = {
-    #"type-h1": ".tui-heading::before"
+    "type-h1": "tui-heading",
+    "type-bold": "tui-bold",
+    "type-italic": "tui-italic",
+    "type-strikethrough": "tui-strike",
+    "hr": "tui-hrline",
+    "blockquote-left": "tui-quote",
+    "list-ul": "tui-ul",
+    "list-ol": "tui-ol",
+    "check2-circle": "tui-task",
+    "text-indent-left": "tui-indent",
+    "text-indent-right": "tui-outdent",
+    "table": "tui-table",
+    "image": "tui-image",
+    "link-45deg": "tui-link",
+    "code": "tui-code",
+    "code-square": "tui-codeblock",
+    "three-dots": "tui-more",
 }
-
 
 with open("bootstrap-icons.css", 'r') as f:
     s = f.read()
-    for new_css_class in css_classes_to_extend:
-        s = s.replace(css_classes_to_extend[new_css_class], new_css_class+" ,")
+    for old_css_class in css_classes_to_extend:
+        s = s.replace(f".bi-{old_css_class}::before", f".{css_classes_to_extend[old_css_class]}::before, .bi-{old_css_class}::before")
 
 with open("bootstrap-icons-patched.css", 'w') as o:
     o.write(s)
-
