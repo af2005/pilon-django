76a6f1dd7dff845ef790d83a94c14197034cec63
af2005 <10899721+af2005@users.noreply.github.com>
2021-04-04 01:34:39 +0200

    tidied up detail view with context

--- www/views/project/wiki/views.py
+++ www/views/project/wiki/views.py
@@ -14,14 +14,6 @@ class ProjectContext(ContextMixin, View):
         return context
 
 
-def _get_page_tree(key) -> dict:
-    wiki_pages = (
-        Project.objects.filter(key=key).first().get_descendants().instance_of(WikiPage)
-    )
-    context = {"nodes": wiki_pages}
-    return context
-
-
 def content_create_with_file(request, key) -> HttpResponse:
     tpl = templates.default_editor(request, title="Create Wiki Page with File", key=key)
     return HttpResponse(tpl)
@@ -32,7 +24,7 @@ class WikiPageDetail(ProjectContext, DetailView):
     template_name = "www/project/wiki/wiki_page_detail.html"
 
 
-class WikiPageCreate(CreateView):
+class WikiPageCreate(ProjectContext, CreateView):
     model = WikiPage
     fields = [
         "parent",
@@ -40,47 +32,13 @@ class WikiPageCreate(CreateView):
     ]
     template_name = "www/project/wiki/wiki_page_create.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        key = self.kwargs.get("key")
-        page_tree = _get_page_tree(key)
-        project = list(Project.objects.filter(key=key))[0]
-        normal_context = {
-            "project": project,
-        }
-        context = {**context, **normal_context, **page_tree}
-        return context
 
-
-class WikiPageUpdate(UpdateView):
+class WikiPageUpdate(ProjectContext, UpdateView):
     model = WikiPage
     fields = ["name"]
     template_name = "www/project/wiki/wiki_page_update.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        key = self.kwargs.get("key")
-        page_tree = _get_page_tree(key)
-        project = list(Project.objects.filter(key=key))[0]
-        page_contents = {
-            "ancestors": self.object.get_ancestors_of_type(WikiPage),
-            "project": project,
-        }
-        context = {**context, **page_tree, **page_contents}
-        return context
 
-
-class WikiHomepage(ListView):
+class WikiHomepage(ProjectContext, ListView):
     model = WikiPage
     template_name = "www/project/wiki/wiki_home.html"
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        key = self.kwargs.get("key")
-        page_tree = _get_page_tree(key)
-        project = list(Project.objects.filter(key=key))[0]
-        normal_context = {
-            "project": project,
-        }
-        context = {**context, **normal_context, **page_tree}
-        return context
