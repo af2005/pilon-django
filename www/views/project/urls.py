6b04855f006268b29c3a7e405414f28b0908b9c9
af2005 <10899721+af2005@users.noreply.github.com>
2021-04-07 21:45:14 +0200

    include urls

--- www/views/project/urls.py
+++ www/views/project/urls.py
@@ -5,12 +5,12 @@ app_name = "project"
 
 urlpatterns = [
     path("<str:key>/", views.ProjectHomepage.as_view(), name="homepage"),
-    path("<str:key>/create/", views.ContentCreate.as_view(), name="create-child-entity"),
+    path("<str:key>/create/", views.ContentCreate.as_view(), name="child-entity-create"),
     path("<str:key>/team/", include("www.views.project.team.urls"), name="team"),
-    path("<str:key>/chat/", views.chat, name="chat"),
+    path("<str:key>/chat/", include("www.views.project.chat.urls"), name="chat"),
     path("<str:key>/tasks/", include("www.views.project.tasks.urls"), name="tasks"),
-    path("<str:key>/calendar/", views.calendar, name="calendar"),
-    path("<str:key>/inventory/", views.inventory, name="inventory"),
+    path("<str:key>/calendar/", include("www.views.project.calendar.urls"), name="calendar"),
+    path("<str:key>/inventory/", include("www.views.project.inventory.urls"), name="inventory"),
     path("<str:key>/wiki/", include("www.views.project.wiki.urls")),
     path("<str:key>/journal/", include("www.views.project.journal.urls")),
 ]
