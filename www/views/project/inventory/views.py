d7be0955d9d96c2b50a1a34751161f4d4b6ed40c
af2005 <10899721+af2005@users.noreply.github.com>
2021-04-07 21:30:59 +0200

    inventory views

--- www/views/project/inventory/views.py
+++ www/views/project/inventory/views.py
@@ -3,28 +3,15 @@ from ..views import ProjectContext
 from www.models import Task
 
 
-class TaskBase(ProjectContext):
+class InventoryBase(ProjectContext):
     model = Task
-    fields = ["parent", "name", "assignee"]
+    fields = []
 
 
-class TaskDetail(TaskBase, DetailView):
-    model = Task
-    template_name = "www/project/tasks/task_detail.html"
-
-
-class TaskCreate(TaskBase, CreateView):
-    template_name = "www/project/tasks/task_create.html"
-
-
-class TaskEdit(TaskBase, UpdateView):
-    template_name = "www/project/tasks/task_edit.html"
-
-
-class TaskList(TaskBase, ListView):
+class InventoryList(InventoryBase, ListView):
     template_name = "www/project/tasks/task_list.html"
     context_object_name = "tasks"
 
 
-class TaskHomepage(TaskList):
+class InventoryHomepage(InventoryList):
     pass
