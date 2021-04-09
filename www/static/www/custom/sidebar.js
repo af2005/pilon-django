9ea2c985e18d4982e69f018d596f1700278bedb3
af2005 <10899721+af2005@users.noreply.github.com>
2021-04-09 19:51:32 +0200

    cleaned up sidebar.js

--- www/static/www/custom/sidebar.js
+++ www/static/www/custom/sidebar.js
@@ -1,21 +1,42 @@
-$(document).ready(function () {
+const sidebar = function () {
+    function toggleCollapsed() {
+         $("#sidebar-wrapper").toggleClass("sidebar-collapsed");
+        $("#sidebar-collapse-toggle i").toggleClass("bi-chevron-double-left").toggleClass("bi-chevron-double-right");
 
+    }
 
-         $(function(){
-            $("#menu-toggle").click(function(e) {
-                e.preventDefault();
-                $("#sidebar-wrapper").toggleClass("toggled");
-            });
+    function extend() {
+        $("#sidebar-wrapper").removeClass("sidebar-collapsed");
+        $("#sidebar-collapse-toggle i").addClass("bi-chevron-double-left").removeClass("bi-chevron-double-right");
+    }
 
-            $(window).resize(function(e) {
-              if($(window).width()<=768){
-                $("#sidebar-wrapper").removeClass("toggled");
-              }else{
-                $("#sidebar-wrapper").addClass("toggled");
-              }
-            });
-          });
+    function collapse() {
+        $("#sidebar-wrapper").addClass("sidebar-collapsed");
+        $("#sidebar-collapse-toggle i").removeClass("bi-chevron-double-left").addClass("bi-chevron-double-right");
+    }
 
+    function initToggleButton() {
+        $("#sidebar-collapse-toggle").click(function () {
+            $("#sidebar-wrapper").toggleClass("sidebar-collapsed");
+            $("#sidebar-collapse-toggle i").toggleClass("bi-chevron-double-left").toggleClass("bi-chevron-double-right");
+        });
+    }
 
-
+    return {
+        toggleCollapsed: toggleCollapsed,
+        extend: extend,
+        collapse: collapse,
+        initToggleButton: initToggleButton
+    }
+}();
+document.addEventListener("DOMContentLoaded", function (event) {
+    sidebar.initToggleButton();
+    $(window).resize(function(e) {
+      if($(window).width()<=768){
+        sidebar.collapse()
+      }
+      else{
+          sidebar.extend();
+      }
+    });
 });
