5284776a830a3d3faaaeeac1ef7ee74b8c6c0b41
af2005 <10899721+af2005@users.noreply.github.com>
2021-04-17 02:13:17 +0200

    wiki page test refactor

--- tests/test_rest/test_wiki_page.py
+++ tests/test_rest/test_wiki_page.py
@@ -12,16 +12,5 @@ class TestWikiPage:
         self.client.login(username="TestUser", password="password")
 
     def test_get(self):
-        response = self.client.get("/rest/entity/")
+        response = self.client.get("/rest/wiki-page/")
         assert response.status_code == 200
-
-    def test_post_only_creating_one(self):
-        pages_before = len(self.client.get("/rest/wiki-page/").json())
-        response = self.client.post(
-            "/rest/wiki-page/",
-            data=json.dumps({"name": "new page", "labels": []}),
-            content_type="application/json",
-        )
-        pages_after = len(self.client.get("/rest/wiki-page/").json())
-        assert response.status_code == 201
-        assert pages_after == pages_before + 1
