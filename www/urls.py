b4ab30afc5dbf7432bf943ff2e5442cf5ff3bcee
af2005 <10899721+af2005@users.noreply.github.com>
2021-04-29 19:45:10 +0200

    react working

--- www/urls.py
+++ www/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", include("www.views.urls")),
     path("rest/", include("www.rest.urls")),
     path("schedule/", include("schedule.urls")),
+    path('hello-webpack/', TemplateView.as_view(template_name='hello_webpack.html'))
+
 ]
