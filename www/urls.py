a58e7c638ed788ea8f1bb8b39cd7858de931e016
af2005 <10899721+af2005@users.noreply.github.com>
2021-04-29 19:48:57 +0200

    moved react app to project

--- www/urls.py
+++ www/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path("", include("www.views.urls")),
     path("rest/", include("www.rest.urls")),
     path("schedule/", include("schedule.urls")),
-    path('hello-webpack/', TemplateView.as_view(template_name='hello_webpack.html'))
 
 ]
