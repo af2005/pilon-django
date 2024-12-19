743e2fbc679c8df9b1b1d6020240cae54dbfb109
af2005 <10899721+af2005@users.noreply.github.com>
2024-12-19 16:48:14 +0100

    sidebar layout working

--- www/static/www/custom/sidebar.js
+++ www/static/www/custom/sidebar.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
+
+         $(function(){
+            $("#menu-toggle").click(function(e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
+
+            $(window).resize(function(e) {
+              if($(window).width()<=768){
+                $("#wrapper").removeClass("toggled");
+              }else{
+                $("#wrapper").addClass("toggled");
+              }
+            });
+          });
+
+
 
 });
