b39cbc014ca78fbca31fa42ada9fd41a61d27be1
af2005 <10899721+af2005@users.noreply.github.com>
2024-12-19 16:50:18 +0100

    sidebar no properly scaled. still not resizeable. Small width nav bar menu broken however

--- www/static/www/custom/sidebar.js
+++ www/static/www/custom/sidebar.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
          $(function(){
             $("#menu-toggle").click(function(e) {
                 e.preventDefault();
-                $("#wrapper").toggleClass("toggled");
+                $("#sidebar-wrapper").toggleClass("toggled");
             });
 
             $(window).resize(function(e) {
               if($(window).width()<=768){
-                $("#wrapper").removeClass("toggled");
+                $("#sidebar-wrapper").removeClass("toggled");
               }else{
-                $("#wrapper").addClass("toggled");
+                $("#sidebar-wrapper").addClass("toggled");
               }
             });
           });
