6f23043f1b48c6bb59d9a43f6c1c83544bea09c8
af2005 <10899721+af2005@users.noreply.github.com>
2024-12-19 16:48:14 +0100

    auth wip

--- www/screens/people.py
+++ www/screens/people.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template import loader
 from . import templates
 
-
+@login_required
 def directory(request):
     tpl = templates.default(request, window_title="People", title="People directory", sidebar=False)
     return HttpResponse(tpl)
