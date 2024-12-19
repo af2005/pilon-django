f9107c833753413b7e14d80870cd7f6a4c4f584a
af2005 <10899721+af2005@users.noreply.github.com>
2024-12-19 16:51:14 +0100

    wip

--- pilon/urls.py
+++ pilon/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('www.urls'))
+    path('', include('www.urls')),
+    path('todo/', include("todo.urls", namespace="todo")),
 ]
