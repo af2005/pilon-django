efea739ef5ed08acaeedfb6d5a9725b758558221
af2005 <10899721+af2005@users.noreply.github.com>
2024-12-19 16:50:19 +0100

    rearranged project files

--- www/screens/templates.py
+++ www/screens/templates.py
@@ -79,3 +79,16 @@ def project_view(request, key, template_name, title, sidebar_items, active_sideb
     return template.render(context, request)
 
 
+def create_page_view(request, key, title, subtitle, sidebar_items):
+    project = list(Project.objects.filter(key=key).values())[0]
+    template = loader.get_template('www/project/create-content.html')
+    context = {
+        'project_key': project["key"],
+        'window_title': f'{title} {project["name"]} ',
+        'page_title': f'{title}',
+        'page_subtitle': f'{subtitle} {project["name"]}',
+        'project': project,
+        'sidebar_items': sidebar_items,
+        'navbar_centertext': project["name"],
+    }
+    return template.render(context, request)
